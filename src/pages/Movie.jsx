6bf4716b2df0228a680b6bd3cf01fcaecb30dd13
Eduardo <educonts08@gmail.com>
2022-09-29 00:40:14 -0300

    done

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,3 +1,79 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill
+} from 'react-icons/bs'
+
+import './Movie.css'
+import { MovieCard } from '../components/MovieCard'
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 export const Movie = () => {
-  return <h1>Movie</h1>
+  const {id} =useParams()
+  const [movie, setMovie] = useState(null)
+
+  const getMovie = async(url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    setMovie(data)
+  }
+
+  function formatCurrency(number) {
+    return number.toLocaleString('en-US', {
+      style:'currency',
+      currency: 'USD'
+    })
+  }
+
+  useEffect(() => {
+    const movieUrl = `${moviesURL}${id}?${apiKey}`
+    getMovie(movieUrl)
+  }, [])
+
+  return <div className="movie_page">
+    {movie && (
+      <>
+      <MovieCard movie={movie} showLink={false} />
+      <p className="tag">{movie.tagline}</p>
+      <div className="info">
+        <h3>
+          <BsWallet2 /> Orçamento:
+        </h3>
+        <p>
+          {formatCurrency(movie.budget)}
+        </p>
+      </div>
+      <div className="info">
+        <h3>
+          <BsGraphUp /> Receita:
+        </h3>
+        <p>
+          {formatCurrency(movie.revenue)}
+        </p>
+      </div>
+      <div className="info">
+        <h3>
+          <BsHourglassSplit /> Duração:
+        </h3>
+        <p>
+          {movie.runtime} minutos
+        </p>
+      </div>
+      <div className="info description">
+        <h3>
+          <BsFillFileEarmarkTextFill /> Descrição:
+        </h3>
+        <p>
+          {movie.overview}
+        </p>
+      </div>
+      </>
+    )}
+  </div>
 }
